4f4a84b4715c19d8bf98f5b9048f628829d1e325
rajeshyadavdotnet <raj@Rajeshs-MacBook-Air.local>
2020-07-22 23:05:42 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+"use strict";
+// Get date of day before yesterday
+var today = new Date();
+var dayBeforeYesterday = new Date(
+  today.setDate(today.getDate() - 2)
+).toLocaleDateString("en-IN", {
+  day: "numeric",
+  month: "short",
+  year: "numeric",
+});
+// Set/append date to html element
+document.querySelector(".js-date").innerHTML = dayBeforeYesterday;
